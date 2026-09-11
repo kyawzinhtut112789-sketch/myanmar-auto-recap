9d55457072a6f5db8ed4d498b629b24b4d40445a
kyawzinhtut112789-sketch <kyawzinhtut112789@gmail.com>
2026-09-11 15:36:19 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "online",
+        "message": "Myanmar Auto Recap Backend is working!"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "ok"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
